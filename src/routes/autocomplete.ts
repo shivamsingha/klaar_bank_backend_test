4010da70e9b628c95d1cccbc37045cf40f8f6fd4
shivam <shivamgit@pm.me>
2021-05-03 13:07:02 +0530

    Fix offset error
    Signed-off-by: shivam <shivamgit@pm.me>

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -9,7 +9,7 @@ router.get('/', paginate.middleware(10, 50), async (req, res) => {
     const q = req.query.q as string;
     const offset = parseInt(req.query.offset as string);
     const result = await prisma.branches.findMany({
-      skip: req.offset || offset,
+      skip: req.offset || offset || 0,
       take: parseInt(req.query.limit as string),
       where: {
         branch: { contains: q, mode: 'insensitive' }
